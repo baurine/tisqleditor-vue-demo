fc4a4889bdc421b72a2a68a26756e2ee5783f3db
baurine <2008.hbl@gmail.com>
2024-07-25 23:07:22 +0800

    test @tidbcloud/tisqleditor-vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import { SQLEditor, EditorCacheProvide } from "@tidbcloud/tisqleditor-vue";
+import { oneDark, bbedit } from "@tidbcloud/codemirror-extension-themes";
+
+const isDark = ref(true);
+function toggle() {
+  isDark.value = !isDark.value;
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <EditorCacheProvide>
+    <button @click="toggle">
+      toggle theme: {{ isDark ? "dark" : "light" }}
+    </button>
+
+    <SQLEditor
+      class="editorContainer"
+      editorId="test"
+      doc="select * from test;"
+      :theme="isDark ? oneDark : bbedit"
+    />
+  </EditorCacheProvide>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.editorContainer {
+  height: 400px;
+  width: 800px;
+  text-align: left;
+  margin-top: 16px;
 }
 </style>
